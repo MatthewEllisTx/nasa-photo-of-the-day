ea271e5ce36a955ac4e117c60ab25ac362075931
Matthew Ellis <mbellistx@gmail.com>
2021-02-20 03:06:23 -0600

    added 10 (usually) most recent images

--- src/components/ContentBoxes.js
+++ src/components/ContentBoxes.js
@@ -14,18 +14,30 @@ const ColumnStyled = styled.div`
 
 export default function ContentBoxes(){
     const [data, setData] = useState([]);
-    
+    const [date, setDate] = useState({ start_date: '', goBack: 10 });
+
+    function getDateInRange(start_date, goBack){
+        const end_date = new Date(Date.now() - goBack * 24 * 60 * 60 * 1000).toISOString().substring(0, 10);
+        setDate({ start_date: end_date, goBack: goBack += 10 });
+        return fetch(`${baseURL}&start_date=${end_date}&end_date=${start_date}`)
+                .then( response => response.json())
+    }
+
     useEffect(() => {
         fetch(baseURL)
             .then( response => response.json())
             .then( response => {
-                
-                const data = [];
-                for(let i = 0; i < 10; i++)
-                    data.push({...response, id: i});
-                setData(data);
-                //setData([response]);
+                return getDateInRange(response.date, date.goBack);
             })
+            .then( response => setData(response.reverse()))
+            // .then( response => {
+                
+            //     const data = [];
+            //     for(let i = 0; i < 10; i++)
+            //         data.push({...response, id: i});
+            //     setData(data);
+                //setData([response]);
+            //})
             .catch( err => console.log(err))
     }, [])
 
